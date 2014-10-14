3eee3e9f5aef133f69262bbf213162188c090ef4
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-14 13:51:38 -0500

    task #1 complete

--- GetOnThatBus/ViewController.m
+++ GetOnThatBus/ViewController.m
@@ -7,21 +7,50 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
-
+@interface ViewController () <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property NSArray *stopArray;
+@property MKPointAnnotation *busStopAnnotation;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self obtainData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)obtainData{
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mobile-makers-lib/bus.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        NSError *jsonError = nil;
+
+        self.stopArray = [((NSDictionary *)[NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]) objectForKey:@"row"];
+
+        //NSLog(@"Connection error: %@", connectionError);
+        //NSLog(@"JSON error: %@", jsonError);
+
+        [self addAnnotations];
+    }];
+}
+
+- (void)addAnnotations{
+    for (NSDictionary *busStop in self.stopArray) {
+        CLLocationCoordinate2D coord;
+        coord.latitude = [[busStop objectForKey:@"latitude"] doubleValue];
+        coord.longitude = [[busStop objectForKey:@"longitude"] doubleValue];
+
+        self.busStopAnnotation = [[MKPointAnnotation alloc] init];
+        self.busStopAnnotation.coordinate = coord;
+        self.busStopAnnotation.title = [busStop objectForKey:@"cta_stop_name"];
+        [self.mapView addAnnotation:self.busStopAnnotation];
+
+        //NSLog(@"adding: %@",self.busStopAnnotation.title);
+    }
 }
 
 @end
